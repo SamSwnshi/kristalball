64c244e8d4d6f31a169348c74a19d3764c558441
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-10-09 20:19:43 +0530

    middleware auth

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+const auth = async (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Access Denied! No Token Provided" });
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId)
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Auth error:", error);
+    return res.status(400).json({ message: "Invalid Token!" });
+  }
+};
+
+export default auth;

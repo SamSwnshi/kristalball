de459ba6bd46536ed10c5b8e2b770855eab23806
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-10-09 20:11:18 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import config from './db/config.js'
 
 dotenv.config()
 const app = express()
@@ -11,4 +12,5 @@ app.use(cors())
 
 app.listen(port,()=>{
     console.log(`Server is Running in PORT: ${port}`)
+    config()
 })
